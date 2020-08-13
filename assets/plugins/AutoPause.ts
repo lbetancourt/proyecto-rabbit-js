1d51f3dca8ca493ea3f9bc77520042919275fb65
Luis Gabriel Betancourt <luis.gabriel@appgate.com>
2020-08-13 12:32:51 -0500

    convert proyect on typescript

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,9 @@
+import MediaPlayer from '../MediaPlayer'
+
 class AutoPause {
+    private threshold: number
+    player: MediaPlayer
+
     constructor() {
         this.threshold = 0.25
         this.handlerIntersection = this.handlerIntersection.bind(this)
@@ -15,9 +20,9 @@ class AutoPause {
         document.addEventListener("visibilitychange", this.handlerVisibilityChange)
     }
 
-    handlerIntersection(entries) {
+    private handlerIntersection(entries:IntersectionObserverEntry[]) {
         const entry = entries[0]
-
+        
         const isVisible = entry.intersectionRatio >= this.threshold
         if (isVisible) {
             this.player.play()
@@ -26,7 +31,7 @@ class AutoPause {
         }
     }
 
-    handlerVisibilityChange() {
+    private handlerVisibilityChange() {
         const isVisible = document.visibilityState === 'visible'
         if (isVisible) {
             this.player.play()
